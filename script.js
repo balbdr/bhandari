dcec1fee4ba90cf3fa4621b2801f7ed611dbaaba
Bal Bahadur Bhandari <148877314+balbdr@users.noreply.github.com>
2023-11-17 13:14:16 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function validate(){
+    let comments=document.getElementById('c');
+    let name=document.getElementById('n');
+    if(comments==""){
+        alert("comments");
+        return false;
+    }
+    if(name==""){
+        alert("name");
+        return false;
+}
+else{
+    document.write(" फारम पेश भयो (your form is submitted)");
+    return true;
+}
+}
