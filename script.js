e759c2fd7c7f4b18661bf4a27b20c98f0fbb907e
Bal Bahadur Bhandari <148877314+balbdr@users.noreply.github.com>
2023-12-03 14:21:53 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function validate(){
     let comments=document.getElementById('c');
     let name=document.getElementById('n');
-    if(comments==""){
+if(comments==""){
         alert("comments");
         return false;
     }
-    if(name==""){
+if(name==""){
         alert("name");
         return false;
 }
 else{
    document.write (" यो फारम अहिले  पेश हुन सकेन ।
              ( This form could not be submitted now  )")
-return true;
+        return true;
 
 }
 }
